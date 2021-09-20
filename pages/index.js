395e2b9e59e5ed116dc32db9fd5976e6fb4c7a0a
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-09-20 22:22:12 +0300

    Header complete

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
+import Header from "../components/Header";
+import Login from "../components/Login";
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
         <title>facebook clone</title>
       </Head>
-      <p>Welcome to facebook clone</p>
+      <Header />
+      <main></main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
