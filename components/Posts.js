f08d77abd078d243619647267d0d8dde6268035a
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-09-22 12:29:20 +0300

    Upload images to storage\ complete

--- components/Posts.js
+++ components/Posts.js
@@ -1,13 +1,25 @@
-import { usCollection } from "react-firebase-hooks";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
 
 export default function Posts() {
-  const [realtimePosts, loading, error] = usCollection(
+  const [realtimePosts] = useCollection(
     db.collection("nextjsposts").orderBy("timestamp", "desc")
   );
-
-  return <div>
-    {realtimePosts.docs.map(post => (
-      <Post/>
-    ))}
-  </div>;
+Post
+  return (
+    <div>
+      {realtimePosts?.docs.map((post) => (
+        <Post
+          key={post.id}
+          name={post.data().name}
+          message={post.data().message}
+          email={post.data().email}
+          timestamp={post.data().timestamp}
+          image={post.data().image}
+          postImage={post.data().postImage}
+        />
+      ))}
+    </div>
+  );
 }
